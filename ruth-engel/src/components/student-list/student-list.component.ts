55dc6c55f6f643fcfa66d23314a5d1e19ce9aa1a
ruthengel <rut150116@gmail.com>
2024-11-17 23:02:46 +0200

    view child

--- ruth-engel/src/components/student-list/student-list.component.ts
+++ ruth-engel/src/components/student-list/student-list.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { student } from '../../student';
 import { StudentDetailsComponent } from '../student-details/student-details.component';
+import { InformationPopupComponent } from '../information-popup/information-popup.component';
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [StudentDetailsComponent],
+  imports: [StudentDetailsComponent, InformationPopupComponent],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.css'
 })
 export class StudentListComponent {
   choice: number = 0;
-  // newstud: student = new student(0, "", 0, false, new Date(), false);
+  @ViewChild(InformationPopupComponent) child: InformationPopupComponent | undefined;
+  @ViewChild('p') childP: ElementRef | undefined;
   arrStudent: student[] = [
     new student(1, "Chaya", 5, true, new Date(), false)
     , new student(2, "suri", 8, false, new Date(), false)
@@ -19,6 +21,12 @@ export class StudentListComponent {
 
   editStudent(item: student) {
     item.flag = true
+  }
+  DeleteStudent() {
+    // if (this.childP != undefined)
+    //   this.childP.nativeElement.innerText = "I changed you"
+    if (this.child != undefined)
+      this.child.showPopup('האם אתה בטוח שברצונך למחוק תלמיד זה')
   }
   addStudent() {
     this.choice = -1
